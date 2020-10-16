caf442175c3780ac867b59ed8c8c5f7e1811ba41
ceylinbrooks <ceylinbrooks@gmail.com>
2020-10-16 16:27:06 -0400

    Updating JS

--- js/app.js
+++ js/app.js
@@ -11,17 +11,12 @@ var seattle = {
   dailyTotal:0,
  
   calcCookiesSoldPerHour: function (){
-    for (var = i=0; i < hours.length; i++){
+    for (var i=0; i < hours.length; i++){
         var numberCustomers = randomCustomer(this.min, this.max);
         var cookiesPerHour = numberCustomers * this.avgCookies;
         console.log(cookiesPerHour);
       }
-        //console.log(`${this.name} sold ${this.numberCustomers});
-
-      }
-    
-    
-  }
+        //console.log(`${this.name} sold ${this.numberCustomers})
 };
 
 //helper function to get random number,
@@ -33,3 +28,23 @@ function randomCookies (min,max){
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
+var parentElement = document.getElementById ('cookie-stand');
+
+var article = document.createElement('article');
+parentElement.appendChild(article);
+
+
+var h2 = document.createElement('h2');
+h2.textContent = seattle.name;
+article.appendChild(h2);
+
+var ul = document.createElement('ul');
+article.appendChild(ul);
+
+for (var i=0; i < seattle.length; i++){
+  var li = document.createElement('li');
+  li.textContent = seattle.hours[i];
+  ul.appendChild(li);
+}
+// var img = document.createElement('img');
+// img.setAttribute('src','images/'+ seattle.name +)
