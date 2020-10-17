e5b4627c4a9188fbf378b0b8ae31b3090f8fed06
ceylinbrooks <ceylinbrooks@gmail.com>
2020-10-17 17:59:20 -0400

    Working on app.js

--- js/app.js
+++ js/app.js
@@ -1,50 +1,45 @@
 'use strict'
 
-var hours = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm'];
+// Hours of Operation
+var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
+var seattleul = document.getElementById('seattle');
 
+// Ceylin - object literals for each store
 var seattle = {
   name: 'Seattle',
-  min:23,
-  max:65,
+  minCust: 23,
+  maxCust: 65,
   avgCookies: 6.3,
   cookiesSoldPerHourArray: [],
-  dailyTotal:0,
- 
-  calcCookiesSoldPerHour: function (){
-    for (var i=0; i < hours.length; i++){
-        var numberCustomers = randomCustomer(this.min, this.max);
-        var cookiesPerHour = numberCustomers * this.avgCookies;
-        console.log(cookiesPerHour);
-      }
-        //console.log(`${this.name} sold ${this.numberCustomers})
+  dailyTotal: 0,
+
+  randomCustomers: function () {
+    return Math.floor(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust);
+  },
+
+  calcCookiesSoldPerHour: function () {
+    for (var i = 0; i, hours.length; i++) {
+      var cookiesPerHour = this.randomCustomers() * this.avgCookies;
+      this.cookiesSoldPerHourArray.push(cookiesPerHour);
+      this.dailyTotal += cookiesPerHour;
+      //console.log('calculate cookies per hour: ', cookiesPerHour);
+    }
+  },
+
+  render: function () {
+    this.cookiesSoldPerHourArray();
+    //console.log('render the list');
+
+    for (var i = 0; i < hours.length; i++) {
+      //create an element
+      var li = document.createElement('li');
+      //give it content
+      li.textContent = `${hours[i]}: ${this.cookiesSoldPerHourArray[i]} cookies`;
+      //Append to the Dom/parentElement
+      seattleul.appendChild(li);
+    }
+  }
+
 };
 
-//helper function to get random number,
-seattle.getCookies();
-
-console.log(seattle);
-
-function randomCookies (min,max){
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
-
-var parentElement = document.getElementById ('cookie-stand');
-
-var article = document.createElement('article');
-parentElement.appendChild(article);
-
-
-var h2 = document.createElement('h2');
-h2.textContent = seattle.name;
-article.appendChild(h2);
-
-var ul = document.createElement('ul');
-article.appendChild(ul);
-
-for (var i=0; i < seattle.length; i++){
-  var li = document.createElement('li');
-  li.textContent = seattle.hours[i];
-  ul.appendChild(li);
-}
-// var img = document.createElement('img');
-// img.setAttribute('src','images/'+ seattle.name +)
+seattle.render();
